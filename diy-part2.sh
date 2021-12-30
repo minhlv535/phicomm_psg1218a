810b899a88dc7b350fc64a4e378d4044743c4cf8
minhlv535 <96867880+minhlv535@users.noreply.github.com>
2021-12-30 17:42:38 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i '/option disabled/d' /etc/config/wireless
+sed -i 's/OpenWrt/Wi-Fi@/g' /etc/config/wireless
+sed -i 's/192.168.1.1/172.16.1.1/g' /etc/config/network
+sed -i '/DISTRIB_REVISION/d' /etc/openwrt_release
+echo "DISTRIB_REVISION='OS'" >> /etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='Router '" >> /etc/openwrt_release

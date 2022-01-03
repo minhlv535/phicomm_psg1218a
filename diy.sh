8691900e6e6f6c7b5998dbd71602c4445f5a5313
minhlv535 <96867880+minhlv535@users.noreply.github.com>
2022-01-03 10:13:22 +0700

    Update and rename diy-part1.sh to diy.sh

---
+++ diy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_generate
+
+# Modify the version number
+# sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/default-settings/files/zzz-default-settings
+
+# 添加新的主题包
+# git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+
+# 去除默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
